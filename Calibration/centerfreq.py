090ccc18d7d43d6e7304ecd3869752a8173a93dc
Brooke Byrne <bbyrne1993@gmail.com>
2025-03-05 12:34:14 -0600

    Updated Files

---
+++ Calibration/centerfreq.py
@@ -0,0 +1,12 @@
+# Perform a frequency sweep or frequency detection routine
+#call the tecmag software to do that, would need a write function?
+
+
+#read the the center frequency from the TNT file
+#center freq is the frequency at which the maximum signal intensity is detected.
+
+
+#store center freq in a variable
+
+#print the center frequency
+
