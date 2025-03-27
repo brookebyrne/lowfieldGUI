2907caee39da2e5704e7395d0b7046db0894bb47
Brooke Byrne <bbyrne1993@gmail.com>
2025-03-27 13:29:11 -0500

    brookeupdate1

--- Calibration/centerfreq.py
+++ Calibration/centerfreq.py
@@ -1,12 +1,42 @@
+import sys
+import os
+import numpy 
+from pytnt import TNTfile
+
+
 # Perform a frequency sweep or frequency detection routine
 #call the tecmag software to do that, would need a write function?
 
 
 #read the the center frequency from the TNT file
+read_tnt_file('FID_grapefruit_20avgs_1.tnt')
+# the output of this is the spectrum data, which is a numpy array of the frequency and intensity and the FID date which 
+#is a numpy array of the time and intensity
+
 #center freq is the frequency at which the maximum signal intensity is detected.
+
+
 
 
 #store center freq in a variable
 
-#print the center frequency
 
+
+def read_tnt_file(file):
+    try:
+        tnt_file = TNTfile(file)
+        fid_data = tnt_file.DATA
+        spec_data = tnt_file.freq_ppm()
+    
+        print("FID Data:")
+        print(fid_data)
+        print("Spectrum Data:")
+        print(spec_data)
+    
+    except FileNotFoundError:
+        print("Error: The file 'your_file.tnt' was not found.")
+    except Exception as e:
+         print(f"An error occurred: {e}")
+    finally:
+        if 'tnt_file' in locals() and hasattr(tnt_file, 'close'):
+            tnt_file.close()
---
+++ Calibration/read_tnt_file.py
@@ -0,0 +1,22 @@
+from pytnt import TNTfile
+
+tnt = TNTfile('GrapefruitTNT/SE_grapefruit_500avgs_2.tnt')
+## can change the file name to any other TNT file in the folder! yay
+
+## The raw NMR FID data
+fid_data = tnt.DATA
+
+fid_data.dtype
+type('complex64')
+fid_data.shape
+(16384, 1, 1, 1)
+
+## The Fourier-transformed spectrum data
+spec_data = tnt.freq_ppm()
+
+spec_data.dtype
+type('float64')
+spec_data.shape
+(16384,)
+
+print(spec_data)
