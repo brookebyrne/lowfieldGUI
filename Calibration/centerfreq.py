eaa3ce24b93d5e3c4fc8d5602f751677316bf36a
Brooke Byrne <bbyrne1993@gmail.com>
2025-03-31 12:24:58 -0500

    a

--- Calibration/centerfreq.py
+++ Calibration/centerfreq.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import numpy 
-from pytnt import TNTfile
+from lowfieldGUI.pytnt_2025.pytnt.processTNT import TNTfile
 
 
 # Perform a frequency sweep or frequency detection routine
@@ -9,7 +9,8 @@ from pytnt import TNTfile
 
 
 #read the the center frequency from the TNT file
-read_tnt_file('FID_grapefruit_20avgs_1.tnt')
+
+
 # the output of this is the spectrum data, which is a numpy array of the frequency and intensity and the FID date which 
 #is a numpy array of the time and intensity
 
@@ -20,23 +21,3 @@ read_tnt_file('FID_grapefruit_20avgs_1.tnt')
 
 #store center freq in a variable
 
-
-
-def read_tnt_file(file):
-    try:
-        tnt_file = TNTfile(file)
-        fid_data = tnt_file.DATA
-        spec_data = tnt_file.freq_ppm()
-    
-        print("FID Data:")
-        print(fid_data)
-        print("Spectrum Data:")
-        print(spec_data)
-    
-    except FileNotFoundError:
-        print("Error: The file 'your_file.tnt' was not found.")
-    except Exception as e:
-         print(f"An error occurred: {e}")
-    finally:
-        if 'tnt_file' in locals() and hasattr(tnt_file, 'close'):
-            tnt_file.close()
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ install pytnt in your local disk by running the following command your terminal:
 update "ThisWorksYay.py" with the name of the TNT file you want to read.
 
 read data from TNT file by running the following command in your terminal:\
-`python ReadData\ThisWorksYay.py`
+`python read_data.py`
 
 
 
